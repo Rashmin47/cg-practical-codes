06980a90474f381d1d01c642a488fa7a5d88e38c
rashmin <rashminxperience@gmail.com>
2026-03-28 10:04:54 +0545

    added 8

--- 8triangle.cpp
+++ 8triangle.cpp
@@ -1,33 +1,39 @@
 #include <stdio.h>
-#include <conio.h>
+#include<conio.h>
 #include <graphics.h>
 
 int main() {
     int gd = DETECT, gm;
 
-    // Original coordinates
+    // Original triangle
     int x1 = 3, y1 = 4;
     int x2 = 6, y2 = 4;
     int x3 = 4, y3 = 8;
 
-    // Reflected coordinates (x-axis)
+    // Reflected triangle (x-axis)
     int x1r = x1, y1r = -y1;
     int x2r = x2, y2r = -y2;
     int x3r = x3, y3r = -y3;
 
+    int xc = 320, yc = 240;  // Screen center
+
     initgraph(&gd, &gm, "C:\\TC\\BGI");
+
+    // Draw X and Y axes
+    line(0, yc, 640, yc);   // X-axis
+    line(xc, 0, xc, 480);   // Y-axis
 
     // Draw original triangle
     setcolor(WHITE);
-    line(x1*20, y1*20, x2*20, y2*20);
-    line(x2*20, y2*20, x3*20, y3*20);
-    line(x3*20, y3*20, x1*20, y1*20);
+    line(xc + x1*20, yc - y1*20, xc + x2*20, yc - y2*20);
+    line(xc + x2*20, yc - y2*20, xc + x3*20, yc - y3*20);
+    line(xc + x3*20, yc - y3*20, xc + x1*20, yc - y1*20);
 
     // Draw reflected triangle
     setcolor(RED);
-    line(x1r*20, y1r*20, x2r*20, y2r*20);
-    line(x2r*20, y2r*20, x3r*20, y3r*20);
-    line(x3r*20, y3r*20, x1r*20, y1r*20);
+    line(xc + x1r*20, yc - y1r*20, xc + x2r*20, yc - y2r*20);
+    line(xc + x2r*20, yc - y2r*20, xc + x3r*20, yc - y3r*20);
+    line(xc + x3r*20, yc - y3r*20, xc + x1r*20, yc - y1r*20);
 
     getch();
     closegraph();
